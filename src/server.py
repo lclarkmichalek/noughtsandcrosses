9a55cc5175429a5b7c36366b82b9ec047a0b277e
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2010-05-11 21:29:44 +0100

    New threaded server. I have plans. This may be big :P

---
+++ src/server.py
@@ -0,0 +1,93 @@
+import select
+import socket
+import threading
+import time
+
+IP = ""
+PORT = 0
+
+class Server(object):
+    def __init__(self, options):
+        self._options = options
+        self._sock = socket.socket(socket.AF_INET,
+                                  socket.SOCK_STREAM)
+        self._threads = ThreadPool()
+        
+        self._sock.bind((IP, PORT))
+        self._sock.listen(self._options["Players"])
+        
+    def run(self):
+        while len(self.Sockets > self._options["Players"]):
+            (clientsocket, address) = self._sock.accept()
+            self._threads.addThread(ClientThread(clientsocket))
+
+
+
+class ThreadPool(object):
+    def __init__(self):
+        self._threads = []
+    
+    def addThread(self, thread):
+        self._threads.append(thread)
+
+
+
+class ClientThread(threading.Thread):
+    def __init__(self, socket):
+        threading.Thread.__init__(self)
+        self._sock = socket
+        self._sock.setblocking(0)
+        self._moving = 0
+    
+    def run(self):
+        while not self._interupted:
+            self._checkinterupted()
+    
+    def interupt(self):
+        self._interupted = True
+    
+    def resume(self):
+        self._interupted = False
+    
+    def _checkinterupted(self):
+        while self._interupted:
+            time.sleep(0.1)
+    
+    def _read(self):
+        if self.check()[0]:
+            self._moving += 1
+            if self._moving >= self._options["Timeout"]:
+                raise RuntimeError
+        else:
+            self._moving = 0
+        while not self._check()[0]:
+            time.sleep(0.1)
+        length = self._sock.recv(2)
+        try:
+            length = int(length)
+        except ValueError:
+            raise RuntimeError
+        content = self._sock.recv(length)
+        return content
+    
+    def _write(self, content):
+        class ToLong(Exception): pass
+        length = len(content)
+        if length > 100:
+            raise ToLong
+        if length < 10:
+            length = '0' + str(length)
+        else:
+            length = str(length)
+        self._sock.send(str(length) + content)
+    
+    def _check(self):
+        timeout = self._options["Timeout"]
+        readable, null, null = select.select([self._sock], [], [], timeout)
+        null, writeable, null = select.select([], [self._sock], [], timeout)
+        null, null, errorable = select.select([], [], [self._sock], timeout)
+        del null
+        
+        if bool(errorable): raise RuntimeError
+        
+        return (bool(readable), bool(writeable))
